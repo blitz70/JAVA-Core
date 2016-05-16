cd3582b0ded266787beb957580f4b1a0620a3ccf
blitz-home <blitz70@hanmail.net>
2016-05-17 04:14:35 +0900

    상수 수업 추가

---
+++ java_tutorials/src/kr/co/iamtek/java_tutorials/constant/Constant.java
@@ -0,0 +1,26 @@
+package kr.co.iamtek.java_tutorials.constant;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		//정수
+		byte 	a1 = 123;
+		short 	a2 = 12345;
+		int 		a3 = 1234567890;
+		long 	a4 = 1234567890123456789L;	//L
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		
+		//실수
+		double b1 = 1.2345678901234567890;
+		float 	b2 = 1.2345678901234567890F;	//F
+		double b3 = 12345678901234567.890;
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		
+	}
+
+}
