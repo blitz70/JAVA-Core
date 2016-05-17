89958f6da85878cba08b16278bfc3652324d9e70
blitz-pcroom <blitz70@hanmail.net>
2016-05-18 04:49:14 +0900

    조건문 수업 추가

---
+++ java_tutorials/src/kr/co/iamtek/java_tutorials/condition/Condition1.java
@@ -0,0 +1,17 @@
+package kr.co.iamtek.java_tutorials.condition;
+
+public class Condition1 {	//if
+
+	public static void main(String[] args) {
+		if(false){
+			System.out.println("true");
+		};
+		if(true){
+			System.out.println(1);
+			System.out.println(2);
+			System.out.println(3);
+			System.out.println(4);
+		};
+		System.out.println(5);
+	};
+}
---
+++ java_tutorials/src/kr/co/iamtek/java_tutorials/condition/Condition2.java
@@ -0,0 +1,12 @@
+package kr.co.iamtek.java_tutorials.condition;
+
+public class Condition2 {	//if, else
+
+	public static void main(String[] args) {
+		if (false) {
+			System.out.println(1);
+		} else {
+			System.out.println(2);
+		};
+	}
+}
