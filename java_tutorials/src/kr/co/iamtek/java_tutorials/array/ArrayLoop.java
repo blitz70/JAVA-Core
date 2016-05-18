810b5e014c2eff946510fdf948e68c715a90f94c
blitz-pcroom <blitz70@hanmail.net>
2016-05-19 03:56:01 +0900

    배열 수업 추가

---
+++ java_tutorials/src/kr/co/iamtek/java_tutorials/array/ArrayLoop.java
@@ -0,0 +1,14 @@
+package kr.co.iamtek.java_tutorials.array;
+
+public class ArrayLoop {
+
+	public static void main(String[] args) {
+		String[] members = {"blitz","oju","curie","김성규"};
+		for (int i = 0; i < members.length; i++) {
+			String member = members[i];
+			System.out.println(member + " 상담 완료.");
+		}
+
+	}
+
+}
---
+++ java_tutorials/src/kr/co/iamtek/java_tutorials/array/Define.java
@@ -0,0 +1,10 @@
+package kr.co.iamtek.java_tutorials.array;
+
+public class Define {
+
+	public static void main(String[] args) {
+		String[] classGroup = {"blitz","oju","curie","김성규"};
+		byte[] classRoom = new byte[10];
+	}
+
+}
