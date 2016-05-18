3a3962f5b0d79cd5d9ae9939e0900558b9540801
blitz-home <blitz70@hanmail.net>
2016-05-18 09:23:09 +0900

    반복문 수업 추가

---
+++ java_tutorials/src/kr/co/iamtek/java_tutorials/loop/While.java
@@ -0,0 +1,11 @@
+package kr.co.iamtek.java_tutorials.loop;
+
+public class While {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 10) {
+			System.out.println(i++);
+		};
+	}
+}
