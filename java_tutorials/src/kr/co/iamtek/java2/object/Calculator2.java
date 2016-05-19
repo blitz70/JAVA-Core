c2fb1548e0a44644584eec946e1f5671542a35e8
blitz-pcroom <blitz70@hanmail.net>
2016-05-20 07:29:16 +0900

    object 수업 추가

---
+++ java_tutorials/src/kr/co/iamtek/java2/object/Calculator2.java
@@ -0,0 +1,13 @@
+package kr.co.iamtek.java2.object;
+
+public class Calculator2 {
+
+	public static void sum(int left, int right) {
+		System.out.println(left + right);
+	};
+	public static void main(String[] args) {
+		sum (10, 20);
+		sum (20, 40);
+	};
+
+}
