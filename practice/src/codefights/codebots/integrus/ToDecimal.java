f8991215ec7e869e5783f58ad8169a080893aa36
blitz-pcroom <blitz70@hanmail.net>
2016-06-17 00:30:19 +0900

    codefights

--- practice/src/codefights/codebots/integrus/ToDecimal.java
+++ practice/src/codefights/codebots/integrus/ToDecimal.java
@@ -4,13 +4,17 @@ public class ToDecimal {
 
 	static int toDecimal(int k, String n) {
 
+		// 4327=(4x10^3)+(3x10^2)+(2x10^1)+(7x10^0) = digit * base ^ power
 		int result = 0;
 		int power = 1;
 		for (int i = n.length() - 1; i >= 0; i--) {
 			String s = ""+ n.charAt(i);
-			int x = Integer.parseInt(s);
-			result = (int) (result + x*Math.pow((double)k, (double)i));
-			System.out.println(result);
+			int digit = Integer.parseInt(s);
+			power = (n.length()) - 1- i;
+			int base = k;
+			int result_temp = (int) (digit * Math.pow(base, power));
+			result += result_temp;
+			//System.out.println(digit + " " + result_temp + " " + result);
 		}
 
 		return result;
@@ -18,8 +22,8 @@ public class ToDecimal {
 
 	public static void main(String[] args) {
 		
-		int answer = toDecimal(10, "87632");
-		//System.out.println(answer);
+		int answer = toDecimal(2, "1101");
+		System.out.println(answer);
 
 	}
 
