f3b56001c7f396ddcb84082d61b291162fdb404b
blitz-pcroom <blitz70@hanmail.net>
2016-07-05 23:31:21 +0900

    DBCP 수업

--- jsp_servlet_study/20_dbcp2/WebContent/memberSelect.jsp
+++ jsp_servlet_study/20_dbcp2/WebContent/memberSelect.jsp
@@ -1,5 +1,5 @@
-<%@page import="java.util.ArrayList"%>
 <%@page import="kr.co.iamtek.*"%>
+<%@page import="java.util.ArrayList"%>
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
--- jsp_servlet_study/20_dbcp2/src/kr/co/iamtek/DbDAO.java
+++ jsp_servlet_study/20_dbcp2/src/kr/co/iamtek/DbDAO.java
@@ -3,13 +3,19 @@ package kr.co.iamtek;
 import java.sql.*;
 import java.util.ArrayList;
 
+import javax.naming.*;
+import javax.sql.DataSource;
+
 public class DbDAO {
 	
 	//DB data
+	private DataSource dataSource;
+	/*
 	private final String DRIVER = "com.mysql.jdbc.Driver";
 	private final String URL = "jdbc:mysql://localhost:3306/jdbc";
 	private final String USER = "blitz";
 	private final String PASSWORD = "mysql";
+	*/
 	private final String TABLE = "members";
 
 	private Connection myConn;
@@ -20,7 +26,10 @@ public class DbDAO {
 	
 	public DbDAO() {
 		try {
-			Class.forName(DRIVER);
+			//Class.forName(DRIVER);
+			System.out.println("test");
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/MySQL"); 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -30,7 +39,8 @@ public class DbDAO {
 		ArrayList<DbDTO> dtos = new ArrayList<DbDTO>();
 		try {
 			SQL = "SELECT * FROM `"+ TABLE + "`";
-			myConn = DriverManager.getConnection(URL, USER, PASSWORD);
+			//myConn = DriverManager.getConnection(URL, USER, PASSWORD);
+			myConn = dataSource.getConnection();
 			myStmt = myConn.createStatement();
 			myRs = myStmt.executeQuery(SQL);
 			while(myRs.next()) {
@@ -59,7 +69,8 @@ public class DbDAO {
 		String str = "";
 		try {
 			SQL = "INSERT INTO `"+ TABLE + "` (id, pw, name, phone, gender) VALUES (?, ?, ?, ?, ?)";
-			myConn = DriverManager.getConnection(URL, USER, PASSWORD);
+			//myConn = DriverManager.getConnection(URL, USER, PASSWORD);
+			myConn = dataSource.getConnection();
 			myPsmt = myConn.prepareStatement(SQL);
 			myPsmt.setString(1, dto.getId());
 			myPsmt.setString(2, dto.getPw());
