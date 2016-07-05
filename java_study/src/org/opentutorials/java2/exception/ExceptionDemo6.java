d405b0f2c6e83041dfb5a04d1672e70b84ab7ca0
blitz-pcroom <blitz70@hanmail.net>
2016-07-05 19:07:02 +0900

    Exception 내용 보충

--- java_study/src/org/opentutorials/java2/exception/ExceptionDemo6.java
+++ java_study/src/org/opentutorials/java2/exception/ExceptionDemo6.java
@@ -9,7 +9,7 @@ class E {
 		throw new ArithmeticException();
 	};
 	
-	void exception2() throws IOException{	//Checked exception
+	void exception2() throws IOException {	//Checked exception
 		throw new IOException();
 	}
 	void exception3(){
@@ -31,8 +31,26 @@ class E {
 	java.lang.Exception
 	java.io.IOException				//checked
 	*/
+	
+}
+public class ExceptionDemo6 {
 
-
-
-
+	public static void main(String[] args) {
+		E e = new E();
+		try {
+			e.exception1();
+		} catch (Exception e2) {
+		}
+		try {
+			e.exception2();
+		} catch (Exception e2) {
+		}
+		try {
+			
+		} catch (Exception e2) {
+			e.exception3();
+		}
+		System.out.println("END");
+	}
+	
 }
