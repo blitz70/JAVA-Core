14b5c5a069482a94ba6a75f08d3f11dbdce044be
blitz-pcroom <blitz70@hanmail.net>
2016-07-17 21:01:18 +0900

    JSP&Servlet Controller 수업 추가

---
+++ jsp_servlet_study/25_controller2/src/kr/co/iamtek/MyFrontController.java
@@ -0,0 +1,63 @@
+package kr.co.iamtek;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class MyFrontController
+ */
+@WebServlet("*.do")
+public class MyFrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyFrontController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doAction(request, response);
+	}
+	
+	private void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = uri.substring(contextPath.length());
+		
+
+		if(command.equals("/membersAll.do")) {
+			response.setContentType("text/html; charset=EUC-KR");
+			PrintWriter writer = response.getWriter();
+			writer.println("<html><head></head><body>");
+			
+			Service service = new MembersAllService();
+
+
+			writer.println("<a href='frontController.jsp'>처음으로</a>");
+			writer.println("</body></html>");
+			writer.close();
+		}
+		
+	}
+
+}
